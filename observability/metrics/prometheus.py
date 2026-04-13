315744b7c5bf289fdfcb0fd0e71733100f683afb
ammmanism <amman.khanansari@gmail.com>
2026-04-13 12:08:48 +0530

    obs: export prometheus metrics for request tracking

--- observability/metrics/prometheus.py
+++ observability/metrics/prometheus.py
@@ -3,7 +3,7 @@ from fastapi import Response
 
 request_counter = Counter('llm_gateway_requests_total', 'Total requests')
 failure_counter = Counter('llm_gateway_failures_total', 'Total failures')
-latency_histogram = Histogram('llm_gateway_request_duration_ms', 'Request latency in ms')
+latency_histogram = Histogram('llm_gateway_request_duration_ms', 'Request latency in ms', buckets=[10, 50, 100, 200, 500, 1000, 2000, 5000])
 
-def metrics_endpoint():
+def metrics_endpoint() -> Response:
     return Response(content=generate_latest(REGISTRY), media_type="text/plain")
