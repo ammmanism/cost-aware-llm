d0b1e371e41d93258c484b0621b31a98785a2992
ammmanism <amman.khanansari@gmail.com>
2026-05-14 19:16:02 +0530

    refactor: resolve linting issues in logging_middleware.py

--- observability/logging_middleware.py
+++ observability/logging_middleware.py
@@ -2,6 +2,7 @@ import json
 import logging
 import time
 import uuid
+
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
